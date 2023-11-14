3502111681d0c3957f2dc57793939900c32f25bf
Kiril Hizhavsky <79530158+qirele@users.noreply.github.com>
2023-11-14 22:59:34 +0100

    add btns for toggling between Celc and Fahr

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const form = document.querySelector('form'); 
 const resultDiv = document.querySelector('#results');
 
+let renderCelcius = true;
+
 form.addEventListener('submit', handleSubmit);
 
 async function handleSubmit(e) {
@@ -14,30 +16,56 @@ async function handleSubmit(e) {
 
   console.clear();
   console.log(originalJson);
+  console.log(processedJson);
   
   render(processedJson);
 }
 
 function render(json) {
-  console.log(json);
-  console.log("Weather for:");
-  console.log(`${json.location.name}, ${json.location.country} ${json.location.region}`);
-  console.log(`Current temperature: ${json.current.temp_c}C / ${json.current.temp_f}F`);
-  console.log(`icon: ${json.current.iconURL}`);
-  for (const hour of json.hour) {
-    console.log(`Hour: ${hour.time.slice(-5)}, temp: ${hour.temp_c}C / ${hour.temp_f}F`);
-  }
-
   while (resultDiv.firstChild) {
     resultDiv.removeChild(resultDiv.firstChild);
   }
 
-  const p1 = createPara(`Location: ${json.location.name}, ${json.location.country}`);
-  const p2 = createPara(`Temperature now: ${json.current.temp_c}C / ${json.current.temp_f}F`);
+  const p1 = createPara(`Results for: ${json.location.name}, ${json.location.country}`);
 
   const div1 = document.createElement("div");
+  div1.className = "current-temp";
+
+  const btnC = document.createElement("button");
+  btnC.innerHTML = "C&deg;"; // forgive me for using this instead of textContent PLEAASE
+  const btnF = document.createElement("button");
+  btnF.innerHTML = "F&deg;"; // how else would i embed html special codes in js??!?! (im sure there is a way)
+  btnC.addEventListener("click", () => {
+    renderCelcius = true;
+    render(json);
+  });
+  btnF.addEventListener("click", () => {
+    renderCelcius = false;
+    render(json);
+  });
+
+  const div2 = document.createElement("div"); 
+  const para1 = createPara("");
+  para1.className = "temp-value";
+  if (renderCelcius) {
+    para1.textContent = json.current.temp_c;
+    btnC.className = "selected";
+  } else {
+    para1.textContent = json.current.temp_f;
+    btnF.className = "selected";
+  }
+  div2.appendChild(para1);
+  div2.appendChild(btnC);
+  div2.appendChild(btnF);
+
+  const imgDiv = document.createElement("div");
   const img = createImg(json.current.iconURL);
-  div1.appendChild(img);
+  imgDiv.appendChild(img);
+
+  div1.appendChild(imgDiv);
+  div1.appendChild(div2);
+
+  const p3 = createPara(json.current.text);
 
   const hourColumns = document.createElement("div");
   hourColumns.className = "hourly";
@@ -45,21 +73,25 @@ function render(json) {
   for (const [idx, hour] of json.hour.entries()) {
     if (idx % 3 === 0) {
       const hourDiv = document.createElement("div");
-      const hourPara1 = createPara(hour.temp_c);
+      const hourPara1 = createPara(`${renderCelcius ? hour.temp_c : hour.temp_f}`);
+      hourPara1.className = "temp-value";
       const hourPara2 = createPara(hour.time.slice(-5));
       const div1 = document.createElement("div");
       const img = createImg(hour.iconURL);
       div1.appendChild(img);
+      const hourPara3 = createPara(hour.text);
+
       hourDiv.appendChild(hourPara1);
       hourDiv.appendChild(hourPara2);
       hourDiv.appendChild(div1);
+      hourDiv.appendChild(hourPara3);
       hourColumns.appendChild(hourDiv);
     }
   }
 
   resultDiv.appendChild(p1);
-  resultDiv.appendChild(p2);
   resultDiv.appendChild(div1);
+  resultDiv.appendChild(p3);
   resultDiv.appendChild(hourColumns);
 }
 
@@ -112,3 +144,5 @@ function createImg(url) {
   img.src = url;
   return img;
 }
+
+
